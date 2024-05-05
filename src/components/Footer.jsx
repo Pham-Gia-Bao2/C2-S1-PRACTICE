dcec41265529986153a44761128c8ed1a31491ca
PhamGiaBao <phamgiabao123abc@gmail.com>
2024-05-05 21:00:00 +0700

    EXERCISE-3

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+// footer component
+export default function Footer() {
+        return (
+            <div className="footer bg">
+                <h2>Footer</h2>
+
+            </div>
+        )
+}
